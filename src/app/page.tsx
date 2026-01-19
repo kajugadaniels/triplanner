e8df0c263984d0914e78eed3efcd0ba9cf552808
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-01-19 19:46:59 +0200

    Setup project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Home = () => {
     return (
         <div>
             Home Page
+            <Button>Click Me</Button>
         </div>
     )
 }
